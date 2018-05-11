8348d40cd86fc6a52e9628399edc1ec472732f7b
Jonathan Beliën <jbe@geo6.be>
2018-05-11 12:43:00 +0200

    Initial commit

---
+++ Geo6.php
@@ -0,0 +1,218 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\Geo6;
+
+use Geocoder\Collection;
+use Geocoder\Exception\InvalidArgument;
+use Geocoder\Exception\InvalidCredentials;
+use Geocoder\Exception\InvalidServerResponse;
+use Geocoder\Exception\QuotaExceeded;
+use Geocoder\Exception\UnsupportedOperation;
+use Geocoder\Http\Provider\AbstractHttpProvider;
+use Geocoder\Model\Address;
+use Geocoder\Model\AddressCollection;
+use Geocoder\Provider\Provider;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+use Http\Client\HttpClient;
+
+/**
+ * @author Jonathan Beliën <jbe@geo6.be>
+ */
+final class Geo6 extends AbstractHttpProvider implements Provider
+{
+    const GEOCODE_ENDPOINT_URL = 'https://api.geo6.be/';
+
+    /**
+     * @var string
+     */
+    private $clientId;
+
+    /**
+     * @var string|null
+     */
+    private $privateKey;
+
+    /**
+     * @param HttpClient $client an HTTP adapter
+     */
+    public function __construct(HttpClient $client, string $clientId, string $privateKey)
+    {
+        $this->clientId = $clientId;
+        $this->privateKey = $privateKey;
+
+        parent::__construct($client);
+    }
+
+    private function getGeocodeEndpointUrl(): string
+    {
+        $url = rtrim(self::GEOCODE_ENDPOINT_URL, '/');
+
+        return $url.'/geocode/getAddressList/%s/%s/%s';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function geocodeQuery(GeocodeQuery $query): Collection
+    {
+        $address = $query->getText();
+
+        $streetName = $query->getData('streetName');
+        $streetNumber = $query->getData('streetNumber');
+        $postalCode = $query->getData('postalCode') ?? null;
+        $locality = $query->getData('locality') ?? null;
+
+        // This API does not support IP
+        if (filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedOperation('The Geo-6 provider does not support IP addresses, only street addresses.');
+        }
+
+        // Save a request if no valid address entered
+        if (empty($address) || empty($streetName) || (empty($postalCode) && empty($locality))) {
+            throw new InvalidArgument('Address, Street Name, and Locality (or Postal Code) cannot be empty.');
+        }
+
+        $url = sprintf($this->getGeocodeEndpointUrl(), urlencode($postalCode ?? $locality), urlencode($streetName), urlencode($streetNumber));
+        $json = $this->executeQuery($url);
+
+        // no result
+        if (empty($json->features)) {
+            return new AddressCollection([]);
+        }
+
+        $results = [];
+        foreach ($json->features as $feature) {
+            $coordinates = $feature->geometry->coordinates;
+
+            foreach ($feature->properties->components as $component) {
+                switch ($component->type) {
+                    case 'municipality':
+                        $municipality = $component->name_fr;
+                        break;
+                    case 'postal_code':
+                        $postalCode = (string) $component->id;
+                        break;
+                    case 'street':
+                        $streetName = $component->name_fr;
+                        break;
+                    case 'street_number':
+                        $streetNumber = (string) $component->name_fr;
+                        break;
+                }
+            }
+
+            $results[] = Address::createFromArray([
+                'providedBy'   => $this->getName(),
+                'latitude'     => $coordinates[1],
+                'longitude'    => $coordinates[0],
+                'streetNumber' => $streetNumber,
+                'streetName'   => $streetName,
+                'locality'     => $municipality,
+                'postalCode'   => $postalCode,
+                'countryCode'  => 'BE',
+            ]);
+        }
+
+        return new AddressCollection($results);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseQuery(ReverseQuery $query): Collection
+    {
+        // This API does not support reverse geocoding
+        if (filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedOperation('The Geo-6 provider does not support reverse geocoding.');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'geo6';
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return \stdClass
+     */
+    private function executeQuery(string $url): \stdClass
+    {
+        $content = $this->getUrlContents($url);
+        $json = json_decode($content);
+        // API error
+        if (!isset($json)) {
+            throw InvalidServerResponse::create($url);
+        }
+
+        return $json;
+    }
+
+    /**
+     * Get URL and return contents. If content is empty, an exception will be thrown.
+     *
+     * @param string $url
+     *
+     * @return string
+     *
+     * @throws InvalidServerResponse
+     */
+    protected function getUrlContents(string $url): string
+    {
+        $token = $this->getToken();
+
+        $request = $this->getMessageFactory()->createRequest('GET', $url, [
+            'Referer'          => 'http'.(!empty($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['SERVER_NAME'].'/',
+            'X-Geo6-Consumer'  => $this->clientId,
+            'X-Geo6-Timestamp' => $token->time,
+            'X-Geo6-Token'     => $token->token,
+        ]);
+        $response = $this->getHttpClient()->sendRequest($request);
+        $statusCode = $response->getStatusCode();
+        if (401 === $statusCode || 403 === $statusCode) {
+            throw new InvalidCredentials();
+        } elseif (429 === $statusCode) {
+            throw new QuotaExceeded();
+        } elseif ($statusCode >= 300) {
+            throw InvalidServerResponse::create($url, $statusCode);
+        }
+        $body = (string) $response->getBody();
+        if (empty($body)) {
+            throw InvalidServerResponse::emptyResponse($url);
+        }
+        return $body;
+    }
+
+
+    public function getToken() {
+        $time = time();
+
+        $t  = $this->clientId.'__';
+        $t .= $time.'__';
+        $t .= parse_url(self::GEOCODE_ENDPOINT_URL, PHP_URL_HOST).'__';
+        $t .= 'GET'.'__';
+        $t .= '/geocode/getAddressList';
+
+        $token = crypt($t, '$6$'.$this->privateKey.'$');
+
+        return (object) [
+            'time'     => $time,
+            'token'    => $token,
+        ];
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# :belgium: [Geocoder PHP](https://github.com/geocoder-php/Geocoder) "Geo-6" provider
+
+[![Latest Stable Version](https://poser.pugx.org/geo6/geocoder-php-geo6-provider/v/stable)](https://packagist.org/packages/geo6/geocoder-php-geo6-provider)
+[![Total Downloads](https://poser.pugx.org/geo6/geocoder-php-geo6-provider/downloads)](https://packagist.org/packages/geo6/geocoder-php-geo6-provider)
+[![Monthly Downloads](https://poser.pugx.org/geo6/geocoder-php-geo6-provider/d/monthly.png)](https://packagist.org/packages/geo6/geocoder-php-geo6-provider)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](LICENSE)
+
+> [Geocoder PHP](https://github.com/geocoder-php/Geocoder) is a PHP library which helps you build geo-aware applications by providing a powerful abstraction layer for geocoding manipulations.
+
+This is the "Geo-6" provider for the [Geocoder PHP](https://github.com/geocoder-php/Geocoder).
+
+**Coverage:** Belgium  
+**API:** <https://api.geo6.be/>
+
+You will require a key to access this API. You can have a look at the available functionalities here : <https://api.geo6.be/>.  
+Please [contact us](https://geo6.be/contact.html) to ask for an access to this API.
+
+## Install
+
+    composer require geo6/geocoder-php-geo6-provider
+
+## Usage
+
+See [Geocoder PHP README file](https://github.com/geocoder-php/Geocoder/blob/master/README.md).
